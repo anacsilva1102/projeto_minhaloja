6716b978c6f3af31564924e6bd1f56910b187605
Ana Clara <ana.c.silva1102@aluno.senai.br>
2026-07-09 16:06:53 -0300

    alterar arquivo

---
+++ js/lista_produtos.js
@@ -0,0 +1,203 @@
+const produtos = [
+
+    {
+        id_produto: 1,
+        descricao_produto: "Conjunto Aurora",
+        valor_unitario: 50.00,
+        unidade: "UN",
+        caminho_imagem: "imagens/Conjunto Aurora.jpg",
+        id_secao: 1,
+        secao: "Conjuntos"
+    },
+
+    {
+        id_produto: 2,
+        descricao_produto: "Conjunto Bella",
+        valor_unitario: 60.00,
+        unidade: "UN",
+        caminho_imagem: "imagens/Conjunto Bella.jpg",
+        id_secao: 1,
+        secao: "Conjuntos"
+    },
+
+    {
+        id_produto: 3,
+        descricao_produto: "Conjunto Camélia",
+        valor_unitario: 35.00,
+        unidade: "UN",
+        caminho_imagem: "imagens/Conjunto Camélia.jpg",
+        id_secao: 1,
+        secao: "Conjuntos"
+    },
+
+    {
+        id_produto: 4,
+        descricao_produto: "Conjunto Flora",
+        valor_unitario: 30.00,
+        unidade: "UN",
+        caminho_imagem: "imagens/Conjunto Flora.jpg",
+        id_secao: 1,
+        secao: "Conjuntos"
+    },
+
+    {
+        id_produto: 5,
+        descricao_produto: "Conjunto Ísis",
+        valor_unitario: 70.00,
+        unidade: "UN",
+        caminho_imagem: "imagens/Conjunto Ísis.jpg",
+        id_secao: 1,
+        secao: "Conjuntos"
+    },
+
+    {
+        id_produto: 6,
+        descricao_produto: "Conjunto Jade",
+        valor_unitario: 130.00,
+        unidade: "UN",
+        caminho_imagem: "imagens/Conjunto Jade.jpg",
+        id_secao: 1,
+        secao: "Conjuntos"
+    },
+
+    {
+        id_produto: 7,
+        descricao_produto: "Conjunto Lavanda",
+        valor_unitario: 50.00,
+        unidade: "UN",
+        caminho_imagem: "imagens/Conjunto Lavanda.jpg",
+        id_secao: 1,
+        secao: "Conjuntos"
+    },
+
+    {
+        id_produto: 8,
+        descricao_produto: "Conjunto Luna",
+        valor_unitario: 135.00,
+        unidade: "UN",
+        caminho_imagem: "imagens/Conjunto Luna.jpg",
+        id_secao: 1,
+        secao: "Conjuntos"
+    },
+
+    {
+        id_produto: 9,
+        descricao_produto: "Conjunto Pérola",
+        valor_unitario: 175.00,
+        unidade: "UN",
+        caminho_imagem: "imagens/Conjunto Pérola.jpg",
+        id_secao: 1,
+        secao: "Conjuntos"
+    },
+
+    {
+        id_produto: 10,
+        descricao_produto: "Conjunto Praia Maré",
+        valor_unitario: 65.00,
+        unidade: "UN",
+        caminho_imagem: "imagens/Conjunto Praia Maré.jpg",
+        id_secao: 1,
+        secao: "Conjuntos"
+    },
+
+    {
+        id_produto: 11,
+        descricao_produto: "Conjunto Verona",
+        valor_unitario: 139.00,
+        unidade: "UN",
+        caminho_imagem: "imagens/Conjunto Verona.jpg",
+        id_secao: 1,
+        secao: "Conjuntos"
+    },
+
+    {
+        id_produto: 12,
+        descricao_produto: "Look Siena",
+        valor_unitario: 155.00,
+        unidade: "UN",
+        caminho_imagem: "imagens/Look Siena.jpg",
+        id_secao: 2,
+        secao: "Looks"
+    },
+
+    {
+        id_produto: 13,
+        descricao_produto: "Vestido Amélia",
+        valor_unitario: 140.00,
+        unidade: "UN",
+        caminho_imagem: "imagens/Vestido Amélia.jpg",
+        id_secao: 3,
+        secao: "Vestidos"
+    },
+
+    {
+        id_produto: 14,
+        descricao_produto: "Vestido Celeste",
+        valor_unitario: 145.00,
+        unidade: "UN",
+        caminho_imagem: "imagens/Vestido Celeste.jpg",
+        id_secao: 3,
+        secao: "Vestidos"
+    },
+
+    {
+        id_produto: 15,
+        descricao_produto: "Vestido de crochê",
+        valor_unitario: 160.00,
+        unidade: "UN",
+        caminho_imagem: "imagens/Vestido de crochê.jpg",
+        id_secao: 3,
+        secao: "Vestidos"
+    },
+
+    {
+        id_produto: 16,
+        descricao_produto: "Vestido Florença",
+        valor_unitario: 150.00,
+        unidade: "UN",
+        caminho_imagem: "imagens/Vestido Florença.jpg",
+        id_secao: 3,
+        secao: "Vestidos"
+    },
+
+    {
+        id_produto: 17,
+        descricao_produto: "Vestido Jasmine",
+        valor_unitario: 145.00,
+        unidade: "UN",
+        caminho_imagem: "imagens/Vestido Jasmine.jpg",
+        id_secao: 3,
+        secao: "Vestidos"
+    },
+
+    {
+        id_produto: 18,
+        descricao_produto: "Vestido Pérola",
+        valor_unitario: 170.00,
+        unidade: "UN",
+        caminho_imagem: "imagens/Vestido Pérola.jpg",
+        id_secao: 3,
+        secao: "Vestidos"
+    },
+
+    {
+        id_produto: 19,
+        descricao_produto: "Vestido Safira",
+        valor_unitario: 165.00,
+        unidade: "UN",
+        caminho_imagem: "imagens/Vestido Safira.jpg",
+        id_secao: 3,
+        secao: "Vestidos"
+    },
+
+    {
+        id_produto: 20,
+        descricao_produto: "Vestido Valentina",
+        valor_unitario: 145.00,
+        unidade: "UN",
+        caminho_imagem: "imagens/Vestido Valentina.jpg",
+        id_secao: 3,
+        secao: "Vestidos"
+    }
+
+];
