3dd6398691ad368f7fe796b0f693272193d8a1be
Ana Clara <ana.c.silva1102@aluno.senai.br>
2026-07-09 16:34:34 -0300

    alterar arquivo

---
+++ js/produtos.js
@@ -0,0 +1,34 @@
+//IMPORTANDO OS PRODUTOS DO ARQUIVO lista_produtos.js
+import { produtos } from './lista_produtos.js'
+
+//PEGANDO ELEMENTOS DO DOM
+const sectionCards = document.querySelector('#cards')
+
+//CARREGANDO OS CARDS
+const listarProdutos = () => {
+
+    produtos.forEach((elem, i) => {
+
+        const divCards = document.createElement('div')
+        divCards.setAttribute('class', 'card')
+    
+        const imgCard = document.createElement('img')
+        imgCard.setAttribute('src', elem.caminho_imagem)
+        imgCard.setAttribute('alt', elem.descricao_produto)
+    
+        const pCard = document.createElement('p')
+        pCard.innerHTML = elem.descricao_produto
+    
+        const h2Card = document.createElement('h2')
+        h2Card.innerHTML = `R$ ${parseFloat(elem.valor_unitario).toFixed(2).replace('.', ',')}`
+    
+        const btnCard = document.createElement('button')
+        btnCard.setAttribute('class', 'btn-add')
+    
+    })
+
+}
+
+
+
+
